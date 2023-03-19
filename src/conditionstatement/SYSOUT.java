54559b8dd85c1f67d604507ce27b9c668813c745
vaisinghequinix <vaisingh@equinix.com>
2023-03-19 19:46:59 +0530

    updates

---
+++ src/conditionstatement/SYSOUT.java
@@ -0,0 +1,22 @@
+package conditionstatement;
+
+public class SYSOUT {
+public static void main(String[] args) {
+	int i = 10;
+	int j = 20;
+	int k = 30;
+	
+	System.out.println(i + j + k);  
+	System.out.println(""+i + j + k);
+	System.out.println(i + j + ""+ k);
+	
+// 	+  append 
+// + plus
+	
+	
+	
+	
+	// until string value + plus , string - append
+	
+}
+}
--- src/conditionstatement/SumOfDigit.java
+++ src/conditionstatement/SumOfDigit.java
@@ -14,10 +14,14 @@ public class SumOfDigit {
 				sum = sum  + (i % 10); // 0 + 5 + 5 + 4 + 1 
 				i = i/10; // 0
 			}
-			System.out.println("Sum : " + sum);	
+			System.out.println("Sum of "+i+" : " + sum);	
 			System.out.println("Give me your input number Please");
 			i = scan.nextInt();
 		}
 		System.out.println("Ok. bye bye tata !");
+		
+		
+		
+		
 	}
 }
