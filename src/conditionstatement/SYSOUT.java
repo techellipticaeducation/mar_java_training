2165833686e3f8f795c93c1dd7ac79a90866578a
vaisinghequinix <vaisingh@equinix.com>
2023-03-19 19:52:15 +0530

    commit

--- src/conditionstatement/SYSOUT.java
+++ src/conditionstatement/SYSOUT.java
@@ -5,18 +5,28 @@ public static void main(String[] args) {
 	int i = 10;
 	int j = 20;
 	int k = 30;
-	
+
+	/*
 	System.out.println(i + j + k);  
 	System.out.println(""+i + j + k);
 	System.out.println(i + j + ""+ k);
-	
+*/
 // 	+  append 
 // + plus
 	
+	showmeyourname();
 	
-	
+	String st = "sdf";
+	st.length();
 	
 	// until string value + plus , string - append
 	
 }
+/**
+ * this is documentation comment
+ */
+public static void showmeyourname() {
+	
+}
+
 }
